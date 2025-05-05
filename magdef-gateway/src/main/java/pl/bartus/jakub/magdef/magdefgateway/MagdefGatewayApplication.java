b900a34be9d207a9bb423f36cf3c9e288a9b8507
DELL <goxer@o2.pl>
2025-05-05 19:24:25 +0200

    init java api module

---
+++ magdef-gateway/src/main/java/pl/bartus/jakub/magdef/magdefgateway/MagdefGatewayApplication.java
@@ -0,0 +1,13 @@
+package pl.bartus.jakub.magdef.magdefgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagdefGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagdefGatewayApplication.class, args);
+    }
+
+}
